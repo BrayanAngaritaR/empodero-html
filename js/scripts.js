2a749dc456a9bc40a1cda748f8b6cc9874919392
Brayan Angarita <brayanangarita11@gmail.com>
2020-05-08 09:45:58 -0500

    Change logo on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,32 @@ $(document).ready(function() {
 	});
 });
 
+// $(function () { 
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 50) { 
+//         	$('.navbar').attr('src', 'http://167.172.224.226/images/logo-white.png');
+//         	$('.navbar').classList.add("fixed-top");
+//         } else {
+//         	
+//         }
+//     })
+// });
+
+$(document).scroll(function(e){
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop > 70){
+        $('.navbar').removeClass('static-top').addClass('fixed-top');
+        $('.navbar').addClass('bg-dark-theme');
+
+
+        $('.navbar-brand img').attr('src', 'http://167.172.224.226/images/logo-white.png');
+
+
+    } else {
+        $('.navbar').removeClass('fixed-top').addClass('static-top');
+        $('.navbar').removeClass('bg-dark-theme');
+        $('.navbar-brand img').attr('src', 'http://167.172.224.226/images/logo.png');
+    }
+});
+
+
